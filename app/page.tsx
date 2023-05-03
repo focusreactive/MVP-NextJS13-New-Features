54b0c22da39cf096b5a82b8741ae2bf720b24760
Sergey <sergey.labuts@gitnation.org>
2023-05-03 16:57:20 +0200

    Eslint, prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+
+import styles from './page.module.css';
 
 export default function Home() {
   return (
@@ -91,5 +92,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
