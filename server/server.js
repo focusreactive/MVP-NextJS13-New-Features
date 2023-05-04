533ef7c01bcfc82e50c6649f45d8177b5ae96053
Sergey <sergey.labuts@gitnation.org>
2023-05-04 17:57:46 +0200

    fix server

--- server/server.js
+++ server/server.js
@@ -6,24 +6,27 @@ http
   .createServer(async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Content-Type', 'application/json');
+    try {
+      if (req.url.includes('/logs')) {
+        res.end(JSON.stringify({ logs }));
+      }
 
-    if (req.url.includes('/logs')) {
-      res.end(JSON.stringify({ logs }));
+      // await new Promise((res) => setTimeout(res, 5000));
+      const url = `https://jsonplaceholder.typicode.com${req.url}`;
+      const counter = (logs[req.url] = (logs[req.url] || 0) + 1);
+
+      // if (logs[url] > 2) {
+      //   res.writeHead(404, { "Content-Type": "text/plain" });
+      //   return res.end();
+      // }
+
+      const result = await fetch(url, { cache: 'no-store' }).then((r) =>
+        r.json(),
+      );
+
+      res.end(JSON.stringify({ counter, data: result, logs }));
+    } catch (error) {
+      console.error(error);
     }
-
-    // await new Promise((res) => setTimeout(res, 5000));
-    const url = `https://jsonplaceholder.typicode.com${req.url}`;
-    const counter = (logs[req.url] = (logs[req.url] || 0) + 1);
-
-    // if (logs[url] > 2) {
-    //   res.writeHead(404, { "Content-Type": "text/plain" });
-    //   return res.end();
-    // }
-
-    const result = await fetch(url, { cache: 'no-store' }).then((r) =>
-      r.json().catch(console.error),
-    );
-
-    res.end(JSON.stringify({ counter, data: result, logs }));
   })
   .listen(4000);
