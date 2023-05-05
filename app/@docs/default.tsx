4deb39932453e345ebf42b7e5d98706d20c6b003
Sergey <sergey.labuts@gitnation.org>
2023-05-05 14:46:11 +0200

    Add default

---
+++ app/@docs/default.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className={'rerender-indicator'}>
+      <h1>Default docs</h1>
+    </div>
+  );
+}
---
+++ app/@examples/global-error.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import Link from 'next/link';
+import { useEffect } from 'react';
+
+export const Error = ({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) => {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <html lang="en">
+      <head />
+
+      <body>
+        <div>
+          <small className={'layout-label'}>root layout</small>
+
+          <nav>
+            <ul>
+              <li>
+                <Link href={'/'}>Home</Link>
+              </li>
+              <li>
+                <Link href={'/posts'}>Posts</Link>
+              </li>
+              <li>
+                <Link href={'/users/list'}>Users</Link>
+              </li>
+              <li>
+                <Link href={'/users/static'}>Users Static</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <main className="main-container">
+            <div className={'rerender-indicator'}>
+              <h2>Something went wrong</h2>
+              <button
+                onClick={
+                  // Attempt to recover by trying to re-render the segment
+                  () => reset()
+                }
+              >
+                Try again
+              </button>
+            </div>
+          </main>
+        </div>
+      </body>
+    </html>
+  );
+};
+
+export default Error;
