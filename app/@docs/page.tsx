c8ab530e689bfab97a2e8b85a590d884b7b0b976
Sergey <sergey.labuts@gitnation.org>
2023-05-05 14:52:18 +0200

    Add page for routes

---
+++ app/@docs/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className={'rerender-indicator'}>
+      <h1>Home Page for docs</h1>
+    </div>
+  );
+};
+
+export default HomePage;
--- app/@examples/page.tsx
+++ app/@examples/page.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <div className={'rerender-indicator'}>
-      <h1>Home Page</h1>
+      <h1>Home Page examples</h1>
     </div>
   );
 };
