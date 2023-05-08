9728e7427b3b798cd0952e4863130e8469885288
Sergey <sergey.labuts@gitnation.org>
2023-05-08 11:47:04 +0200

    Add missing pages

---
+++ app/@examples/users/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'Users',
+};
+
+const Page = () => {
+  return (
+    <div className={'rerender-indicator'}>
+      <h1>Users page</h1>
+    </div>
+  );
+};
+
+export default Page;
