91c45dac8dc4a058e9927ba0f1397ecfb35c5347
Eugene Boruhov <eugene@focusreactive.com>
2023-05-11 23:01:40 +0200

    Revert test changes

--- app/static/page.tsx
+++ app/static/page.tsx
@@ -1,19 +1,8 @@
 import Doc from './doc.mdx';
 
-import type { Metadata, ResolvingMetadata } from 'next';
-
-type Props = {
-  params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
+export const metadata = {
+  title: 'Static + Cache examples',
 };
-
-export async function generateMetadata({ params, searchParams }: Props) {
-  console.log({ params, searchParams });
-
-  return {
-    title: searchParams.wow || 'string',
-  };
-}
 
 const Page = () => {
   return <Doc />;
