9083a002bbf306a46b228045f8f944eab4ff7d9f
Eugene Boruhov <eugene@focusreactive.com>
2023-05-15 18:14:07 +0200

    Cleanup API module

--- utils/api.ts
+++ utils/api.ts
@@ -4,34 +4,34 @@ import type { Post, User } from '@/app/global';
 
 const fetchData = async (url: string, options?: RequestInit) => {
   const result = await proxyFetch.fetch(url, options).catch(console.error);
-  return { data: result?.data, counter: result?.counter };
+  return { data: result?.data };
 };
 
 export const api = {
   post: async (id: string, options?: RequestInit) => {
     const result = await fetchData(`/posts/${id}`, options);
 
-    return [result?.data, result?.counter] as [Post, number];
+    return [result?.data] as [Post];
   },
 
   posts: async () => {
     const result = await fetchData('/posts');
-    return [result.data, result.counter] as [Post[], number];
+    return [result.data] as [Post[]];
   },
 
   user: async (id: string) => {
     const result = await fetchData(`/users/${id}`);
-    return [result.data, result.counter] as [User, number];
+    return [result.data] as [User];
   },
 
   users: async () => {
     const result = await fetchData('/users');
-    return [result.data, result.counter] as [User[], number];
+    return [result.data] as [User[]];
   },
 
   images: async (options?: RequestInit) => {
     const result = await fetchData('/images', options);
 
-    return [result.data, result.counter] as [string, number];
+    return [result.data] as [string];
   },
 };
--- utils/proxyFetch.ts
+++ utils/proxyFetch.ts
@@ -1,41 +1,24 @@
-import { clear, read, save } from './handleState';
-
 export const proxyFetch = (() => {
   async function proxy(url: string, options?: RequestInit) {
-    const logs = await read();
-    const counter = (logs[url] = (logs[url] || 0) + 1);
-
     try {
-      if (url.includes('/logs')) {
-        return logs;
-      }
-
-      if (url.includes('/clear-logs')) {
-        return clear();
-      }
-
       if (url.includes('/images')) {
         const apiHost = `https://dog.ceo/api/breeds/image/random`;
         const result = await fetch(apiHost, options);
         const data = await result.json();
 
-        save(logs);
-
-        return { counter, data: data?.message, logs };
+        return { data: data?.message };
       }
 
       const apiHost = `https://jsonplaceholder.typicode.com${url}`;
       const result = await fetch(apiHost, options);
       const data = await result.json();
 
-      save(logs);
-
-      return { counter, data, logs };
+      return { data };
     } catch (error) {
       console.error(error);
     }
 
-    return { counter, data: [], logs };
+    return { data: [] };
   }
 
   return {
