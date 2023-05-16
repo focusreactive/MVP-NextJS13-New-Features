cb1ab3994ed395f1653da6671f9fcfab7a81ba00
Eugene Boruhov <eugene@focusreactive.com>
2023-05-16 21:57:42 +0200

    Force light theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <head />
 
       <body style={{ border: 0 }}>
