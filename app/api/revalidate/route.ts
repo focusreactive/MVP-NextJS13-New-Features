97bd8df7d09bd5ff4f4bd42405db9e25a94e58f5
Sergey <sergey.labuts@gitnation.org>
2023-05-18 10:31:35 +0200

    Patch build time

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -7,5 +7,6 @@ export async function GET(request: NextRequest) {
   const path = request.nextUrl.searchParams.get('path') || '/';
 
   revalidatePath(path);
+
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
--- components/BuildInfo/BuildInfo.tsx
+++ components/BuildInfo/BuildInfo.tsx
@@ -5,7 +5,7 @@ export default function BuildInfo() {
   return (
     <aside className={styles.stamp}>
       {/* <div style={{ backgroundColor: getRandomColor() }}>RANDOM COLOR</div> */}
-      <div>{`Built at: ${new Date(Date.now()).toISOString()}`}</div>
+      <div>{`Built at: ${new Date(Date.now() + 7200000).toISOString()}`}</div>
       {/* <div>NEXT_RUNTIME: {process.env.NEXT_RUNTIME}</div> */}
     </aside>
   );
