a7d90b10d3341bc1ce1755ca6c979950b329a98a
Sergey <sergey.labuts@gitnation.org>
2023-05-19 09:27:31 +0200

    Not found root

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ export const metadata = {
 export default function NotFound() {
   return (
     <div>
-      <h1>Not found – 404!</h1>
+      <h1>Not found ROOT– 404!</h1>
       <div>
         <Link href="/">Go back to Home</Link>
       </div>
